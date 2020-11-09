39561bcfb16bd86497b465b7488748d31bc37b38
Maria <maria.mihaila.17@gmail.com>
2020-11-09 17:20:08 +0200

    Call handleSubmit method and onSearchChange in correct places

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -13,13 +13,17 @@ export default class SearchForm extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
+        this.props.onSearch(this.state.searchText);
         e.currentTarget.reset();
     }
 
     render() {
         return (
-            <form className="search-form">
-                <input type="search" name="search" placeholder="Search" required/>
+            <form className="search-form" onSubmit={this.handleSubmit}>
+                <input type="search"
+                       onChange={this.onSearchChange}
+                       name="search"
+                       placeholder="Search..." required/>
                 <button type="submit" className="search-button">
                     <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
                         <path
