eabccc84d15abd8a6d5e93c78d406ada12073a2e
Maria <maria.mihaila.17@gmail.com>
2020-11-09 16:09:54 +0200

    Install React Router and create routing and exact path for NotFound page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import {
+    BrowserRouter,
+    Route
+} from 'react-router-dom';
 import './App.css';
 import SearchForm from "./SearchForm";
 import Nav from "./MainNav";
@@ -6,17 +10,20 @@ import PhotoContainer from "./PhotoContainer";
 import NotFound from "./NotFound";
 import Config from "./config";
 
+
 let apiKey = Config;
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <SearchForm />
-                <Nav />
-                <PhotoContainer />
-                <NotFound />
-            </div>
+            <BrowserRouter>
+                <div className="container">
+                    <SearchForm  />
+                    <Nav />
+                    <PhotoContainer />
+                    <Route exact path="/notFound" component={NotFound}/>
+                </div>
+            </BrowserRouter>
         );
     }
 }
