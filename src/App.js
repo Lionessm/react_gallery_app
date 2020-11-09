904bea6dd7b092ae9e16d0d2807dfe14a0e3031b
Maria <maria.mihaila.17@gmail.com>
2020-11-09 14:01:34 +0200

    Create SearchForm component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          REEEACTTTT SHOULD BE LEARNED
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SearchForm from './SearchForm';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <SearchForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
