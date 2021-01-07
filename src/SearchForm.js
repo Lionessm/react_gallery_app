9f6eeb2601c325ec92ca0af2b8cb1038f46006a5
Maria <maria.mihaila.17@gmail.com>
2021-01-07 15:34:08 +0200

    Search form outputs to console the searched term

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -13,10 +13,7 @@ export default class SearchForm extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        console.log("this.state.searchText "  , this.state.searchText)
-        this.props.onSearch(this.state.searchText);
-
-        e.currentTarget.reset();
+        console.log("this.state.searchText", this.state.searchText)
     }
 
     render() {
