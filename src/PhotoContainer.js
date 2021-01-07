ef3566be5fc45595cad101a14e2ef2eb89a16f6e
Maria <maria.mihaila.17@gmail.com>
2021-01-07 16:03:44 +0200

    Removed ont wanted code

--- src/PhotoContainer.js
+++ src/PhotoContainer.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Photo from './Photo';
-import Flickr from "flickr-sdk";
 import apiKey from "./config";
 import request from "sync-request";
 
 const PhotoContainer = (props) => {
+    // Get the search query from props or use the default `cats` value.
     const searchQuery = props.match.params.searchQuery ? props.match.params.searchQuery : 'cats';
 
+    // Set document title.
+    document.title = searchQuery;
+
     // Get the photos from the API in a sync way.
-    const res = request('GET', `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&per_page=5&tags=${searchQuery}&format=json&nojsoncallback=1`);
+    const res = request('GET', `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&per_page=25&tags=${searchQuery}&format=json&nojsoncallback=1`);
     const response = res.getBody();
     const photos = JSON.parse(response).photos.photo;
 
@@ -29,34 +32,6 @@ const PhotoContainer = (props) => {
             </ul>
         </div>
     );
-}
-
-const performSearch = async (searchQuery) => {
-    let flickr = new Flickr(apiKey);
-
-    const response = await flickr.photos.search({
-        tags: searchQuery
-    });
-
-    return await callMap(response);
-}
-
-const callMap = async (response) => {
-    let flickr = new Flickr(apiKey);
-
-    // This map returns an array of photos with their URL and ID
-    const photosWithURL = await Promise.all(response.body.photos.photo.map(async (photo) => {
-        const res = await flickr.photos.getSizes({
-            photo_id: photo.id
-        });
-
-        return {
-            url: res.body.sizes.size[1].source,
-            id: photo.id
-        };
-    }));
-
-    return photosWithURL;
 }
 
 export default PhotoContainer;
